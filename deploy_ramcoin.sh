ac6b3d2281972b3d6346be23b56990631110ed5a
Linh B Ngo <lngo@wcupa.edu>
2022-03-10 07:31:29 -0500

    Update deploy_ramcoin.sh

--- deploy_ramcoin.sh
+++ deploy_ramcoin.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -x
 namespaceStatus=$(kubectl get namespaces ramcoin -o json | jq .status.phase -r)
 
 if [ $namespaceStatus == "Active" ]
@@ -8,10 +8,15 @@ then
     kubectl delete namespaces ramcoin
 fi
 
+echo "Creating namespace ramcoin"
 kubectl create namespace ramcoin 
  
+echo "Creating pods"
 kubectl create -f ramcoin.yaml --namespace ramcoin
+
+echo "Creating services"
 kubectl create -f ramcoin-service.yaml --namespace ramcoin
+
 
 kubectl get pods -n ramcoin
 
